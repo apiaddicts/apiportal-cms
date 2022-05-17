222eaab9daf615f410b900325721e3c58e89f025
EnriqueTun23 <EnriqueTun23>
2022-05-17 14:26:07 -0500

    fix(models): We changed the way of uploading images to local and made adjustments to the models and eliminated some collections.

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,23 @@
 module.exports = ({ env }) => ({
-       upload: {
-        provider: "aws-s3-cloudfront",
+      //  upload: {
+      //   provider: "aws-s3-cloudfront",
+      //   providerOptions: {
+      //     accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      //     secretAccessKey: env("AWS_ACCESS_SECRET"),
+      //     region: env("AWS_REGION"),
+      //     params: {
+      //       Bucket: env("AWS_BUCKET"),
+      //     },
+      //     cdn: env('CLOUDFRONT')
+
+      //   },
+      // },
+    upload: {
+      config: {
+        provider: 'local',
         providerOptions: {
-          accessKeyId: env("AWS_ACCESS_KEY_ID"),
-          secretAccessKey: env("AWS_ACCESS_SECRET"),
-          region: env("AWS_REGION"),
-          params: {
-            Bucket: env("AWS_BUCKET"),
-          },
-          cdn: env('CLOUDFRONT')
-          
+          sizeLimit: 100000,
         },
       },
-   });
+    },
+  });
