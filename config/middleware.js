130763bff17b4d6a35d139e3421450d4eb8e99d6
Walther Chambilla <walther.chambilla.pe@cloudappi.net>
2023-03-23 20:56:50 +0000

    Update 2 files
    - /config/middleware.js
    - /extensions/upload/middlewares/upload/defaults.json

--- config/middleware.js
+++ config/middleware.js
@@ -3,6 +3,8 @@ module.exports = {
     parser: {
       enabled: true,
       multipart: true,
+      formLimit: "200mb",
+      jsonLimit: "200mb",
       formidable: {
         maxFileSize: 200 * 1024 * 1024 // Defaults to 200mb
       }
