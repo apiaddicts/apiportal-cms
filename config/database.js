9c8b06f0d6dda2897b6d6b723ba8cb9d9215ef2f
apiquality-support <support@apiquality.io>
2025-04-10 12:11:51 -0500

    dababase migration config

--- config/database.js
+++ config/database.js
@@ -1,19 +1,15 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: env("DATABASE_CLIENT"),
-        host: env("DATABASE_HOST"),
-        port: env.int("DATABASE_PORT"),
-        database: env("DATABASE_NAME"),
-        username: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
-        // schema: env("DATABASE_SCHEMA"),
-        ssl:  false,
-      },
-      options: {}
+  connection: {
+    client: 'postgres',
+    connection: {
+      client: env("DATABASE_CLIENT"),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      username: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl:  false,
     },
+    debug: false
   },
 });
