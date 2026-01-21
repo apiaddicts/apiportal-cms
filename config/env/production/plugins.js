ac546f73abbc122cc4cb55682b06472d0aa30c7c
Melsy Huamani <melsyhuamanivargas@gmail.com>
2026-01-21 15:26:45 -0500

    fix: configure email provider for development environment

---
+++ config/env/production/plugins.js
@@ -0,0 +1,40 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        rootPath: env("AWS_FOLDER", 'assets'),
+        s3Options: {
+          creadentials : {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_ACCESS_SECRET'),
+          },
+          region: env('AWS_REGION'),
+          params: {
+            ACL: 'private',
+            Bucket: env('AWS_BUCKET'),
+          },
+        }
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+  email: {
+    config: {
+      provider: 'amazon-ses',
+      providerOptions: {
+        key: env('AWS_ACCESS_KEY_ID_SES'),
+        secret: env('AWS_SECRET_ACCESS_KEY_SES'),
+        amazon: env('AWS_URL_SES'),
+      },
+      settings: {
+        defaultFrom: env('EMAIL_DEFAULT_FROM'),
+        defaultReplyTo: env('EMAIL_DEFAULT_FROM'),
+      },
+    },
+  },
+});
