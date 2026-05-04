af5e5f08a87ce15fb0e8735bad8a48ff03114823
Julian <julian.salcedo.co@cloudappi.net>
2026-05-04 18:19:11 -0500

    feat(config): pin Node 20 y añadir branch SQLite para desarrollo local

--- config/database.js
+++ config/database.js
@@ -1,22 +1,39 @@
 const fs = require('fs');
+const path = require('path');
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: env("DATABASE_CLIENT"),
+module.exports = ({ env }) => {
+  const client = env('DATABASE_CLIENT', 'sqlite');
+
+  if (client === 'sqlite') {
+    return {
+      connection: {
+        client: 'sqlite',
+        connection: {
+          filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        },
+        useNullAsDefault: true,
+      },
+    };
+  }
+
+  return {
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
-      schema: env("DATABASE_SCHEMA", 'public'),
-      app: {
-        keys: env.array('APP_KEYS'),
+      client,
+      connection: {
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        user: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        schema: env('DATABASE_SCHEMA', 'public'),
+        app: {
+          keys: env.array('APP_KEYS'),
+        },
+        ssl: env.bool('DATABASE_SSL', false) && {
+          ca: fs.readFileSync(`${__dirname}/ca-certificate.crt`).toString(),
+        },
       },
-      ssl: env.bool("DATABASE_SSL", false) && {
-        ca: fs.readFileSync(`${__dirname}/ca-certificate.crt`).toString()
-      },
+      debug: false,
     },
-    debug: false
-  },
-});
+  };
+};
